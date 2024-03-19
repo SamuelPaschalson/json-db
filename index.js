a56fa6c7273044a1b451e612a8a3962758ed35a3
SamuelPaschalson <samuelpaschalson@gmail.com>
2024-03-19 05:55:44 -0700

    json-db

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const Schema = require('./lib/schema');
-const Model = require('./lib/model');
 
-class jsonApi {
-  static Schema(definition) {
-    return new Schema(definition);
-  }
+'use strict';
 
-  static model(name, schema) {
-    return new Model(name, schema);
-  }
-}
+module.exports = require('./lib/json-db');
 
-module.exports = jsonApi;
-
-
-// module.exports = {
-//   Schema: require('./lib/schema'),
-//   Model: require('./lib/model'),
-//   // Add other exports as needed
-// };
